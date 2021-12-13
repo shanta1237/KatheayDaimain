3b24b6c3f0e54d5aabd1f59522b365ef2a3aac52
unknown <shanta.khanal09@gmail.com>
2021-12-13 09:30:52 +0545

    Created admin and user different login page in html nad css and implement it into the katheaydai project[C

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -9,7 +10,7 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class Admin extends Authenticatable
 {
     use HasApiTokens;
     use HasFactory;
@@ -24,11 +25,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
         'phone',
+        'email',
         'password',
-        'provider',
-        'provider_id',
     ];
 
     /**
