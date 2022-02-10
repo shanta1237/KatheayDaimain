4718dd13fb9a2a2643ecac3762e581b20a7010da
47sanish <78839614+47sanish@users.noreply.github.com>
2022-02-10 18:22:35 +0545

    user can like the product

---
+++ resources/views/pages/wishlist.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('layouts.menubar')
+    <link rel="stylesheet" type="text/css" href="{{ asset('frontend/styles/cart_styles.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('frontend/styles/cart_responsive.css') }}">
+
+    <div class="cart_section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="cart_container">
+                        <div class="cart_title">Your Wishlist List</div>
+                        <div class="cart_items">
+                            <ul class="cart_list">
+                                @foreach ($product as $row)
+                                    <li class="cart_item clearfix">
+                                        <div class="cart_item_image"><img src="{{ asset($row->image_one) }}" alt="">
+                                        </div>
+                                        <div class="cart_item_info d-flex flex-md-row flex-column justify-content-between">
+                                            <div class="cart_item_name cart_info_col">
+                                                <div class="cart_item_title">Name</div>
+                                                <div class="cart_item_text">{{ $row->product_name }}</div>
+                                            </div>
+                                            @if ($row->product_color == null)
+                                            @else
+                                                <div class="cart_item_color cart_info_col">
+                                                    <div class="cart_item_title">Color</div>
+                                                    <div class="cart_item_text">{{ $row->product_color }}</div>
+                                                </div>
+                                            @endif
+                                            @if ($row->product_size == null)
+
+                                            @else
+                                                <div class="cart_item_color cart_info_col">
+                                                    <div class="cart_item_title">Size</div>
+                                                    <div class="cart_item_text">{{ $row->product_size }}</div>
+                                                </div>
+                                            @endif
+                                            <div class="cart_item_total cart_info_col">
+                                                <div class="cart_item_title">Action</div><br>
+                                                <a href="#" class="btn btn-sm btn-danger">X</a>
+                                            </div>
+                                        </div>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
